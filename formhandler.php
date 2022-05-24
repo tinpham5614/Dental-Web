63d5b918e51fc30b19fb893546feaa9c64339310
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2022-05-23 23:17:18 -0700

    Add files via upload

---
+++ formhandler.php
@@ -0,0 +1,38 @@
+<?php
+
+$host = 'dbcontact.mysql.database.azure.com';
+$username = 'qwe';
+$password = 'Tinpham4491!';
+$db_name = 'dbcontact';
+
+$conn = mysqli_init();
+
+
+// Establish the connection
+mysqli_real_connect($conn, 'dbcontact.mysql.database.azure.com', 'qwe', 'Seattle5614!', 'dbcontact', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$txtName = $_POST['Name'];
+$txtEmail = $_POST['mail'];
+$txtPhone = $_POST['Phone'];
+$txtMessage = $_POST['comments'];
+
+$sql = "INSERT INTO `tbl_contact` (`ID`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+if ($conn->query($sql) === TRUE) {
+  header('Location: thx.html');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+
+
+
+?>
---
+++ formhandler1.php
@@ -0,0 +1,45 @@
+<?php
+
+$host = 'dbcontact.mysql.database.azure.com';
+$username = 'qwe';
+$password = 'Tinpham4491!';
+$db_name = 'dbcontact';
+
+$conn = mysqli_init();
+
+
+// Establish the connection
+mysqli_real_connect($conn, 'dbcontact.mysql.database.azure.com', 'qwe', 'Seattle5614!', 'dbcontact', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$txtName = $_POST['Name'];
+$txtEmail = $_POST['mail'];
+$txtPhone = $_POST['Phone'];
+$txtMessage = $_POST['comments'];
+$check="";
+foreach($textCheck as $check1)
+    {
+        $check .= $check1.",";
+    }
+$type = $_POST['patient'];
+
+$sql = "INSERT INTO `tbl_contact` (`ID`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`,`fldTime`,`fldType`) VALUES 
+('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage','$check','$type')";
+
+if ($conn->query($sql) === TRUE) {
+  header('Location: thx1.html');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+
+
+
+?>
