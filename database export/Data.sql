18f03920c3edfb39b350b9e3a3ca9b65dea7620f
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2022-06-07 21:41:07 -0700

    Add files via upload

---
+++ database export/Data.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: dbcontact.mysql.database.azure.com    Database: dbcontact
+-- ------------------------------------------------------
+-- Server version	5.7.37-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_contact`
+--
+
+DROP TABLE IF EXISTS `tbl_contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_contact` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `fldName` varchar(50) DEFAULT NULL,
+  `fldEmail` varchar(100) DEFAULT NULL,
+  `fldPhone` varchar(15) DEFAULT NULL,
+  `fldTime` varchar(30) DEFAULT NULL,
+  `fldType` varchar(100) DEFAULT NULL,
+  `fldMessage` text,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_contact`
+--
+
+LOCK TABLES `tbl_contact` WRITE;
+/*!40000 ALTER TABLE `tbl_contact` DISABLE KEYS */;
+INSERT INTO `tbl_contact` VALUES (1,'John Smith','john.smith@gmail.com','2061111111','afternoon,evening,','New Patient','Good morning!!'),(2,'','john.smith@gmail.com','',NULL,NULL,''),(3,'Adam Smith','adam.smith@gmail.com','2062222222','afternoon,','Checkup','Good afternoon!!'),(4,'Chris Jackson','chris.jackson@gmail.com','2063333333','evening,','Comprehensive','Good Evening!');
+/*!40000 ALTER TABLE `tbl_contact` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-11 11:03:47
